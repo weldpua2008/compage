2543b98651f30e2ca8e8a3438d62476e5ff55e48
Valery Solovyov <weldpua2008@gmail.com>
2017-02-03 22:33:58 +0000

    adding detecting of DISPLAY

--- lib/process.sh
+++ lib/process.sh
@@ -8,3 +8,16 @@ process_get_pids_byname(){
 	$SUDO ps waxl|grep 'sshd.*tty'|grep -v grep|awk '{print $3}'	
 }
 
+##################################
+# find $DISPLAY for process
+##################################
+process_get_dispay(){
+    #pid=$(ps   -C $process_name -o pid=|head -1| tr -d ' ') 
+    local pid=${1:-}
+    DISPLAY=$($SUDO cat /proc/$PID/environ 2> /dev/null | strings | awk 'BEGIN{FS="=";} $1=="DISPLAY" {print $2; exit}')
+    [ "$DISPLAY" = "" ] && return 1
+    echo "$DISPLAY"
+    return 0
+}
+
+
